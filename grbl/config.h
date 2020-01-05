144b05c08c06116b28a9104f22c696643c5f05c6
GowanR <gowan.rowland@gmail.com>
2020-01-04 20:55:11 -0700

    reconfigured limit bits to prevent microstepping interferance

--- grbl/config.h
+++ grbl/config.h
@@ -35,7 +35,7 @@
 // one configuration file by placing their specific defaults and pin map at the bottom of this file.
 // If doing so, simply comment out these two defines and see instructions below.
 #define DEFAULTS_GENERIC
-#define CPU_MAP_2560_INITIAL
+#define CPU_MAP_2560_KIP33
 
 // To use with RAMPS 1.4 Board, comment out the above defines and uncomment the next two defines
 // #define DEFAULTS_RAMPS_BOARD
